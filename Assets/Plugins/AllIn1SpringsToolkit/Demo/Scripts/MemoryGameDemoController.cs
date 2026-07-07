142b97fe703bddf19c8cdaed9649717bd1ffd020
Zohaib_Shahjehan <zohaib.shahjahan@finz.io>
2026-07-07 20:13:27 +0500

    RandomUnity Changes

--- Assets/Plugins/AllIn1SpringsToolkit/Demo/Scripts/MemoryGameDemoController.cs
+++ Assets/Plugins/AllIn1SpringsToolkit/Demo/Scripts/MemoryGameDemoController.cs
@@ -8,13 +8,13 @@ namespace AllIn1SpringsToolkit.Demo.Scripts
         //When a tile is clicked, it will flip and show its icon. If two tiles are clicked and they have the same icon, they will stay open
         //If they don't have the same icon, they will close again
         //The game is won when all the tiles are open
-        
+
         //Spring wise, we have a TransformSpringComponent for the grid, and a TransformSpringComponent for each tile
         //The grid will scale up when the game is won
         //The tiles will rotate when they are clicked
-        
+
         //The class MemoryTile will handle the tile behavior
-        
+
         [Space, Header("Memory Tiles")]
         [SerializeField] private MemoryTile[] memoryTiles;
         [SerializeField] private Sprite[] icons;
@@ -23,7 +23,7 @@ namespace AllIn1SpringsToolkit.Demo.Scripts
         [Space, Header("Other")]
         [SerializeField] private TransformSpringComponent gridTransformSpring;
         [SerializeField] private float winScaleNudge;
-        
+
         private MemoryTile firstTileClicked;
         private MemoryTile secondTileClicked;
         private int solvedPairs;
@@ -40,27 +40,27 @@ namespace AllIn1SpringsToolkit.Demo.Scripts
             //Shuffle memory tiles and then assign the memoryGameId to each tile and their corresponding icon and color
             ShuffleMemoryTiles();
 
-            for(int i = 0; i < memoryTiles.Length; i++)
+            for (int i = 0; i < memoryTiles.Length; i++)
             {
                 memoryTiles[i].memoryGameId = Mathf.FloorToInt(i / 2f);
                 memoryTiles[i].SetIcon(icons[memoryTiles[i].memoryGameId], colors[memoryTiles[i].memoryGameId]);
                 memoryTiles[i].memoryGameDemoController = this;
             }
-            
+
             isRandomizingTiles = false;
         }
 
         public void TileClicked(MemoryTile tileClicked)
         {
-            if(isRandomizingTiles)
+            if (isRandomizingTiles)
             {
                 return;
             }
-            
-            if(firstTileClicked != null && secondTileClicked != null)
+
+            if (firstTileClicked != null && secondTileClicked != null)
             {
                 //Check if the two tiles are the same
-                if(firstTileClicked.memoryGameId != secondTileClicked.memoryGameId)
+                if (firstTileClicked.memoryGameId != secondTileClicked.memoryGameId)
                 {
                     //If they are not the same, we close them
                     firstTileClicked.FaceDown();
@@ -69,20 +69,20 @@ namespace AllIn1SpringsToolkit.Demo.Scripts
                     secondTileClicked = null;
                 }
             }
-            
+
             //If we have no tiles clicked, we set the clicked tile as the first one
-            if(firstTileClicked == null)
+            if (firstTileClicked == null)
             {
                 firstTileClicked = tileClicked;
                 firstTileClicked.FaceUp();
             }
             //If we have the first tile clicked, we set the clicked tile as the second one
-            else if(secondTileClicked == null)
+            else if (secondTileClicked == null)
             {
                 secondTileClicked = tileClicked;
                 secondTileClicked.FaceUp();
-                
-                if(firstTileClicked.memoryGameId == secondTileClicked.memoryGameId)
+
+                if (firstTileClicked.memoryGameId == secondTileClicked.memoryGameId)
                 {
                     //If they are the same, we keep them open and discard the clicked tiles
                     firstTileClicked = null;
@@ -91,14 +91,14 @@ namespace AllIn1SpringsToolkit.Demo.Scripts
                 }
             }
         }
-        
+
         public void CheckIfGameIsWon()
         {
-            if(solvedPairs == (memoryTiles.Length / 2))
+            if (solvedPairs == (memoryTiles.Length / 2))
             {
                 solvedPairs = 0;
                 gridTransformSpring.AddVelocityScale(Vector3.one * winScaleNudge);
-                for(int i = 0; i < memoryTiles.Length; i++)
+                for (int i = 0; i < memoryTiles.Length; i++)
                 {
                     memoryTiles[i].ResetTiles();
                 }
@@ -113,7 +113,7 @@ namespace AllIn1SpringsToolkit.Demo.Scripts
         {
             System.Random random = new System.Random();
             int n = memoryTiles.Length;
-    
+
             for (int i = n - 1; i > 0; i--)
             {
                 int j = random.Next(0, i + 1);
--- Assets/Plugins/AllIn1SpringsToolkit/Demo/Scripts/MemoryTile.cs
+++ Assets/Plugins/AllIn1SpringsToolkit/Demo/Scripts/MemoryTile.cs
@@ -9,14 +9,14 @@ namespace AllIn1SpringsToolkit.Demo.Scripts
         //It handles the rotation of the spring
         //Holds the state data of the tile
         //It receives the click callback and sends it to the MemoryGameDemoController
-        
+
         [SerializeField] private Image iconImage;
         [SerializeField] private Image ringImage;
         [SerializeField] private Image backImage;
         [SerializeField] private GameObject backfaceGameObject;
         [SerializeField] private TransformSpringComponent transformSpring;
         public int memoryGameId;
-        
+
         private Transform visualsTransform;
         [HideInInspector] public MemoryGameDemoController memoryGameDemoController;
         [HideInInspector] public bool isFaceUp;
@@ -47,7 +47,7 @@ namespace AllIn1SpringsToolkit.Demo.Scripts
             bool isFrontFace = dotProduct >= 0;
             backfaceGameObject.SetActive(!isFrontFace);
         }
-        
+
         public void SetIcon(Sprite icon, Color iconColor)
         {
             iconImage.sprite = icon;
@@ -55,22 +55,22 @@ namespace AllIn1SpringsToolkit.Demo.Scripts
             ringImage.color = iconColor;
             backImage.color = Color.Lerp(iconColor, Color.white, 0.85f);
         }
-        
+
         public void FaceUp()
         {
             isFaceUp = true;
             transformSpring.SetTargetRotation(new Vector3(0f, 0f, 0f));
         }
-        
+
         public void FaceDown()
         {
             isFaceUp = false;
             transformSpring.SetTargetRotation(new Vector3(0f, 180f, 0f));
         }
-        
+
         public void OnClick()
         {
-            if(isFaceUp)
+            if (isFaceUp)
             {
                 memoryGameDemoController.CheckIfGameIsWon();
                 return;
